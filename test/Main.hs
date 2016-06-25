9c409aad75098e7ffef16a6fb815060e61755d37
NCrashed <ncrashed@gmail.com>
2016-06-25 20:43:46 +0300

    Test client that writes eventlog to file

--- test/Main.hs
+++ test/Main.hs
@@ -6,5 +6,13 @@ import Debug.Trace
 import Control.Monad 
 import Control.Concurrent
 
+import Test.Client
+import System.Socket.Family.Inet6
+
+import System.Directory
+
 main = bracket (initLiveProfile defaultLiveProfileOpts) stopLiveProfile $ const $ do
+  flag <- doesFileExist "test.eventlog"
+  when flag $ removeFile "test.eventlog"
+  recieveRemoteEventlog (SocketAddressInet6 inet6Loopback 8242 0 0) "test.eventlog"
   void $ replicateM 100000 $ traceEventIO "MyEvent"
---
+++ test/Test/Client.hs
@@ -0,0 +1,26 @@
+module Test.Client(
+    recieveRemoteEventlog
+  ) where
+
+import Control.Monad 
+import Control.Concurrent
+import Control.Exception
+import System.IO 
+
+import System.Socket
+import System.Socket.Family.Inet6
+import System.Socket.Type.Stream
+import System.Socket.Protocol.TCP
+
+import qualified Data.ByteString as BS 
+
+type ServerSocket = Socket Inet6 Stream TCP 
+
+recieveRemoteEventlog :: SocketAddress Inet6 -> FilePath -> IO ()
+recieveRemoteEventlog addr filename = void . forkIO $ bracket socket close $ 
+  \s -> connect s addr >> go s 
+  where 
+  go :: ServerSocket -> IO ()
+  go s = withFile filename WriteMode $ \h -> forever $ do 
+    bs <- receive s 1024 mempty
+    BS.hPut h bs 
