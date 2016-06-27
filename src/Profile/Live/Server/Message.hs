fb57eca1232ca3fb18a32e1120adaff840aedb6a
NCrashed <ncrashed@gmail.com>
2016-06-28 01:37:35 +0300

    Refactor collector step function, use MonadWriter and MonadState

--- src/Profile/Live/Server/Message.hs
+++ src/Profile/Live/Server/Message.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Profile.Live.Server.Message(
     ProfileMsg(..)
   , ServiceMsg(..)
@@ -13,13 +14,15 @@ module Profile.Live.Server.Message(
   ) where 
 
 import Control.DeepSeq
-import Control.Monad.Trans.State.Strict
+import Control.Monad.State.Class
+import Control.Monad.Writer.Class
 import Data.Monoid
 import Data.Time 
 import Data.Word 
 import GHC.Generics
 import GHC.RTS.Events
 import GHC.RTS.EventsIncremental
+import System.Log.FastLogger
 
 import qualified Data.Foldable as F 
 import qualified Data.ByteString as BS 
@@ -249,128 +252,132 @@ emptyMessageCollector timeout = MessageCollector {
   , collectorTimeout = timeout
   }
 
--- | Handle event's block messages. Watch when a block starts, when messages
--- of the block are arrived. The main nuance is that header and events can 
--- arrive in out of order, so need to handle this neatly.   
-collectorBlock :: UTCTime -- ^ Current time
-  -> EventParserState -- ^ Incremental parser with fully feeded header
-  -> EventBlockMsg -- ^ Message about event block
-  -> State MessageCollector (Maybe (S.Seq Event))
-collectorBlock curTime parser msg = case msg of 
-  EventBlockMsgHeader header@EventBlockMsgData{..} -> do 
-    modify' $ \mc@MessageCollector{..} -> mc {
-        collectorBlocks = case eblockMsgDataId `H.lookup` collectorBlocks of 
-          Nothing -> H.insert eblockMsgDataId (curTime, Just header, S.empty) collectorBlocks
-          Just (t, _, es) -> H.insert eblockMsgDataId (t, Just header, es) collectorBlocks
-      }
-    return Nothing 
-  EventBlockMsgPart {..} -> do 
-    mc <- get 
-    mpayload <- collectorPartial curTime parser eblockMsgPayload 
-    case mpayload of 
-      Nothing -> return Nothing -- Could be only a part of whole event
-      Just payload -> case eblockMsgId `H.lookup` collectorBlocks mc of 
-        Nothing -> do 
-          put mc {
-              collectorBlocks = H.insert eblockMsgId (curTime, Nothing, S.singleton payload) $ collectorBlocks mc
-            }
-          return Nothing
-        Just (t, Nothing, es) -> do
-          put mc {
-              collectorBlocks = H.insert eblockMsgId (t, Nothing, es S.|> payload) $ collectorBlocks mc
-            }
-          return Nothing
-        Just (t, Just header, es) -> if fromIntegral (S.length es) >= eblockMsgDataEventsCount header
-          then do
-            modify' $ \mc'@MessageCollector{..} -> mc' {
-                collectorBlocks = H.delete eblockMsgId collectorBlocks
-              }
-            let blockHead = Event {
-                  evTime = eblockMsgDataBeginTimestamp header
-                , evSpec = EventBlock {
-                    end_time = eblockMsgDataEndTimestamp header
-                  , cap = capToGhcEvents $ eblockMsgDataCap header
-                  , block_size = eblockMsgDataEventsCount header
-                  }
-                , evCap = Just $ capToGhcEvents $ eblockMsgDataCap header
-                }
-            return . Just $ (blockHead S.<| es) S.|> payload
-          else do 
-            modify' $ \mc'@MessageCollector{..} -> mc' {
-                collectorBlocks = H.insert eblockMsgId (t, Just header, es S.|> payload) collectorBlocks
-              }
-            return Nothing 
-
--- | Process partial messages and return collected results. 
--- The main nuance is that header and events can arrive in 
--- out of order, so need to handle this neatly.   
-collectorPartial :: UTCTime -- ^ Current time
-  -> EventParserState -- ^ Incremental parser with fully feeded header
-  -> EventMsgPartial -- ^ Message about event block
-  -> State MessageCollector (Maybe Event)
-collectorPartial curTime parser msg = case msg of 
-  EventMsgFull ev -> return $ Just ev 
-  EventMsgPartial header@EventPartialData{..} -> do 
-    mc@MessageCollector{..} <- get
-    put mc {
-        collectorPartials = case epartialMsgId `H.lookup` collectorPartials of 
-          Nothing -> H.insert epartialMsgId (curTime, Just header, S.empty) collectorPartials
-          Just _ -> collectorPartials
-      }
-    return Nothing 
-  EventMsgPart {..} -> do 
-    mc@MessageCollector{..} <- get
-    case epartMsgId `H.lookup` collectorPartials of 
-      Nothing -> do 
-        put mc {
-            collectorPartials = H.insert epartMsgId 
-              (curTime, Nothing, S.singleton (epartMsgNum, epartMsgPayload)) collectorPartials
-          }
-        return Nothing
-      Just (t, Nothing, es) -> do 
-        put mc {
-            collectorPartials = H.insert epartMsgId 
-              (t, Nothing, (epartMsgNum, epartMsgPayload) `orderedInsert` es) collectorPartials
-          }
-        return Nothing
-      Just (t, Just header, es) -> if fromIntegral (S.length es) >= epartialMsgParts header
-        then case parsed of 
-          Incomplete -> return Nothing -- something went wrong. TODO: log the staff 
-          Complete -> return Nothing -- something went wrong. TODO: log the staff 
-          ParseError _ -> return Nothing  -- something went wrong. TODO: log the staff 
-          Item ev -> do
-            put mc {
-              collectorPartials = H.delete epartMsgId collectorPartials
-              }
-            return $ Just ev
-        else do 
-          put mc {
-              collectorPartials = H.insert epartMsgId 
-                (t, Just header, (epartMsgNum, epartMsgPayload) `orderedInsert` es) collectorPartials
-            }
-          return Nothing
-        where
-          es' = (epartMsgNum, epartMsgPayload) `orderedInsert` es 
-          payload = F.foldl' (\acc s -> acc <> s) BS.empty $ snd `fmap` es'
-          (parsed, _) = readEvent $ pushBytes parser payload -- We drop new parser state as we always 
-            -- feed enough data to get new event. TODO: check if parser can be used as state of the collector
 
 -- | Perform one step of converting the profiler protocol into events
-stepMessageCollector :: UTCTime -- ^ Current time
+stepMessageCollector :: (MonadState MessageCollector m, MonadWriter LogStr m)
+  => UTCTime -- ^ Current time
   -> EventParserState -- ^ Incremental parser with fully feeded header
   -> EventMsg -- ^ New message arrived
-  -> MessageCollector -- ^ Current state of collector
-  -> (Maybe (S.Seq Event), MessageCollector) -- ^ Result of decoded event and next state of the collector
-stepMessageCollector curTime parser msg collector = me `deepseq` collector' `deepseq` (me, collector')
+  -> m (Maybe (S.Seq Event)) -- ^ Result of decoded event if any
+stepMessageCollector curTime parser msg = case msg of
+  EventBlockMsg bmsg -> collectorBlock bmsg 
+  EventMsg pmsg -> do 
+    res <- collectorPartial pmsg 
+    return $ case res of 
+      Nothing -> Nothing 
+      Just e -> Just $ S.singleton e 
   where 
-    (me, collector') = runState step collector
-    step = case msg of
-      EventBlockMsg bmsg -> collectorBlock curTime parser bmsg 
-      EventMsg pmsg -> do 
-        res <- collectorPartial curTime parser pmsg 
-        return $ case res of 
-          Nothing -> Nothing 
-          Just e -> Just $ S.singleton e 
+  -- Handle event's block messages. Watch when a block starts, when messages
+  -- of the block are arrived. The main nuance is that header and events can 
+  -- arrive in out of order, so need to handle this neatly.   
+  collectorBlock :: (MonadState MessageCollector m, MonadWriter LogStr m)
+    => EventBlockMsg -- ^ Message about event block
+    -> m (Maybe (S.Seq Event))
+  collectorBlock bmsg = case bmsg of 
+    EventBlockMsgHeader header@EventBlockMsgData{..} -> do 
+      modify' $ \mc@MessageCollector{..} -> mc {
+          collectorBlocks = case eblockMsgDataId `H.lookup` collectorBlocks of 
+            Nothing -> H.insert eblockMsgDataId (curTime, Just header, S.empty) collectorBlocks
+            Just (t, _, es) -> H.insert eblockMsgDataId (t, Just header, es) collectorBlocks
+        }
+      return Nothing
+    EventBlockMsgPart {..} -> do 
+      mc <- get 
+      mpayload <- collectorPartial eblockMsgPayload 
+      case mpayload of 
+        Nothing -> return Nothing -- The case when only part of a message arrived
+        Just payload -> case eblockMsgId `H.lookup` collectorBlocks mc of 
+          Nothing -> do 
+            put mc {
+                collectorBlocks = H.insert eblockMsgId (curTime, Nothing, S.singleton payload) $ collectorBlocks mc
+              }
+            return Nothing
+          Just (t, Nothing, es) -> do
+            put mc {
+                collectorBlocks = H.insert eblockMsgId (t, Nothing, es S.|> payload) $ collectorBlocks mc
+              }
+            return Nothing
+          Just (t, Just header, es) -> if fromIntegral (S.length es) >= eblockMsgDataEventsCount header
+            then do
+              modify' $ \mc'@MessageCollector{..} -> mc' {
+                  collectorBlocks = H.delete eblockMsgId collectorBlocks
+                }
+              let blockHead = Event {
+                    evTime = eblockMsgDataBeginTimestamp header
+                  , evSpec = EventBlock {
+                      end_time = eblockMsgDataEndTimestamp header
+                    , cap = capToGhcEvents $ eblockMsgDataCap header
+                    , block_size = eblockMsgDataEventsCount header
+                    }
+                  , evCap = Just $ capToGhcEvents $ eblockMsgDataCap header
+                  }
+              return . Just $ (blockHead S.<| es) S.|> payload
+            else do 
+              modify' $ \mc'@MessageCollector{..} -> mc' {
+                  collectorBlocks = H.insert eblockMsgId (t, Just header, es S.|> payload) collectorBlocks
+                }
+              return Nothing
+
+  -- | Process partial messages and return collected results. 
+  -- The main nuance is that header and events can arrive in 
+  -- out of order, so need to handle this neatly.   
+  collectorPartial :: (MonadState MessageCollector m, MonadWriter LogStr m)
+    => EventMsgPartial -- ^ Message about event block
+    -> m (Maybe Event)
+  collectorPartial pmsg = case pmsg of 
+    EventMsgFull ev -> return $ Just ev 
+    EventMsgPartial header@EventPartialData{..} -> do 
+      mc@MessageCollector{..} <- get
+      put mc {
+          collectorPartials = case epartialMsgId `H.lookup` collectorPartials of 
+            Nothing -> H.insert epartialMsgId (curTime, Just header, S.empty) collectorPartials
+            Just _ -> collectorPartials
+        }
+      return Nothing 
+    EventMsgPart {..} -> do 
+      mc@MessageCollector{..} <- get
+      case epartMsgId `H.lookup` collectorPartials of 
+        Nothing -> do 
+          put mc {
+              collectorPartials = H.insert epartMsgId 
+                (curTime, Nothing, S.singleton (epartMsgNum, epartMsgPayload)) collectorPartials
+            }
+          return Nothing
+        Just (t, Nothing, es) -> do 
+          put mc {
+              collectorPartials = H.insert epartMsgId 
+                (t, Nothing, (epartMsgNum, epartMsgPayload) `orderedInsert` es) collectorPartials
+            }
+          return Nothing
+        Just (t, Just header, es) -> if fromIntegral (S.length es) >= epartialMsgParts header
+          then case parsed of 
+            Incomplete -> do
+              tell $ "Received incomplete message: " <> toLogStr (show payload) <> ", please report a bug.\n"
+              return Nothing -- something went wrong.
+            Complete -> do
+              tell $ "Received event, but the incremental parser is finished, please report a bug.\n"
+              return Nothing -- something went wrong.
+            ParseError s -> do
+              tell $ "Received incorrect event's payload: " <> toLogStr (show payload) 
+                <> ". Error: " <> toLogStr s <> "\n"
+              return Nothing  -- something went wrong. 
+            Item ev -> do
+              put mc {
+                collectorPartials = H.delete epartMsgId collectorPartials
+                }
+              return $ Just ev
+          else do 
+            put mc {
+                collectorPartials = H.insert epartMsgId 
+                  (t, Just header, (epartMsgNum, epartMsgPayload) `orderedInsert` es) collectorPartials
+              }
+            return Nothing
+          where
+            es' = (epartMsgNum, epartMsgPayload) `orderedInsert` es 
+            payload = F.foldl' (\acc s -> acc <> s) BS.empty $ snd `fmap` es'
+            (parsed, _) = readEvent $ pushBytes parser payload -- We drop new parser state as we always 
+              -- feed enough data to get new event. Pure parser state cannot be stored as collector state
+              -- as the bits could arrive in out of order.
 
 -- | Inserts the pair into sequence with preserving ascending order
 orderedInsert :: Ord a => (a, b) -> S.Seq (a, b) -> S.Seq (a, b)
