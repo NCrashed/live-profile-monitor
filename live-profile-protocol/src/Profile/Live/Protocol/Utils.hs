fd4f7569cacc51f5a7cdb2090f469a581b06284c
NCrashed <ncrashed@gmail.com>
2016-08-23 19:10:28 +0300

    Fix bugs wit occaisonal fails in send/receive pipeline

--- live-profile-protocol/src/Profile/Live/Protocol/Utils.hs
+++ live-profile-protocol/src/Profile/Live/Protocol/Utils.hs
@@ -28,18 +28,16 @@ import Control.Exception
 import Control.Monad.Except 
 import Data.Binary.Serialise.CBOR
 import Data.Monoid 
-import Data.Storable.Endian
 import Data.Word
-import Foreign hiding (void)
-import Foreign.C.Types 
 import GHC.Conc.Sync (labelThread)
 import GHC.Generics
 import System.Log.FastLogger
 import System.Socket 
 
+import qualified Data.Binary.Get as G 
+import qualified Data.Binary.Put as P
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BS 
-import qualified Data.ByteString.Unsafe as BS 
 
 import Profile.Live.Protocol.Message 
 import Profile.Live.Protocol.State
@@ -52,9 +50,11 @@ data ReceiveMsgError =
 
 -- | Helper to receive all requested bytes
 receiveWaitAll :: Socket i str p -> Int -> IO B.ByteString 
+--receiveWaitAll p size = receive p size (msgNoSignal <> msgWaitAll)
 receiveWaitAll p size = go mempty size
   where 
   go !bs !n 
+    | n < 0 = fail "Received too much"
     | n > 0 = do 
       bs' <- receive p n msgNoSignal
       go (bs <> bs') (n - B.length bs')
@@ -65,11 +65,12 @@ recieveMessage :: Socket i str p -> IO (Either ReceiveMsgError ProfileMsg)
 recieveMessage p = runExceptT $ do 
   lbytes <- liftIO $ receiveWaitAll p 4
   guardEndOfInput lbytes
-  guardLength 4 lbytes 
-  (l :: Word32) <- liftIO $ BS.unsafeUseAsCString lbytes $ peekBE . castPtr
+
+  let (l :: Word32) = G.runGet G.getWord32be $ BS.fromStrict lbytes
+  liftIO $ putStrLn $ "Receiving " <> show l <> " bytes... " <> show lbytes 
+  
   msgbytes <- liftIO $ receiveWaitAll p (fromIntegral l)
   guardEndOfInput msgbytes 
-  guardLength (fromIntegral l) msgbytes
   case deserialiseOrFail $ BS.fromStrict msgbytes of 
     Left er -> throwError . MsgDeserialisationFail $ "Failed to deserialize message: " 
         <> showl er <> ", payload: " <> showl msgbytes
@@ -77,20 +78,25 @@ recieveMessage p = runExceptT $ do
   where 
   guardEndOfInput bs | B.null bs = throwError MsgEndOfInput
                      | otherwise = return ()
-  guardLength n bs 
-    | B.length bs == n = return ()
-    | otherwise = throwError $ MsgDeserialisationFail $ "Not matched length of message: "
-      <> "expected " <> showl n <> ", recieved " <> showl (B.length bs)
+
+-- | Send all messages to remote host
+sendAll :: Socket i str p -> B.ByteString -> IO ()
+sendAll p = go
+  where 
+  go !bs 
+    | B.null bs = return ()
+    | otherwise = do 
+      i <- send p bs msgNoSignal
+      go (B.drop (fromIntegral i) bs)
 
 -- | Helper to write message into socket
-sendMessage :: Socket i str p -> ProfileMsg -> IO ()
+sendMessage :: Socket i str p -> ProfileMsg -> IO () 
 sendMessage p msg = do
-  let msgbytes = serialise msg 
-  let lbytes = fromIntegral (BS.length msgbytes) :: Word32
-  allocaArray 4 $ \(ptr :: Ptr CUChar) -> do 
-    pokeBE (castPtr ptr) lbytes
-    lbs <- BS.fromStrict <$> BS.unsafePackCStringLen (castPtr ptr, 4)
-    void $ send p (BS.toStrict $ lbs <> msgbytes) msgNoSignal
+  let bytes = serialise msg 
+  let l = fromIntegral (BS.length bytes) :: Word32
+  let lbytes = P.runPut $ P.putWord32be l
+  let fullmsg = lbytes <> bytes
+  sendAll p (BS.toStrict fullmsg)
 
 -- | Helper that prints all exceptions passed through
 printExceptions :: String -> IO a -> IO a 
